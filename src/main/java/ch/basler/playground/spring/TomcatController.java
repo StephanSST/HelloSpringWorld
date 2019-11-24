524df919eb2223b009cabb50b17574d2e5c3deba
stephan <stephan.staeheli@gmx.ch>
2019-11-24 20:50:49 +0100

    initial add

---
+++ src/main/java/ch/basler/playground/spring/TomcatController.java
@@ -0,0 +1,14 @@
+package ch.basler.playground.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TomcatController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+
+}
