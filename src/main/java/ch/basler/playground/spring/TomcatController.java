a38665d3a9e36bae5a160af48a2faa6478ddd2c1
stephan <stephan.staeheli@gmx.ch>
2019-11-25 21:31:26 +0100

    Message extended

--- src/main/java/ch/basler/playground/spring/TomcatController.java
+++ src/main/java/ch/basler/playground/spring/TomcatController.java
@@ -8,7 +8,7 @@ public class TomcatController {
 
   @RequestMapping("/")
   public String index() {
-    return "Greetings from Spring Boot!";
+    return "Greetings from Spring Boot! Hello Again!";
   }
 
 }
